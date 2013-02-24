cfcaa20ffed19502528ad419110533c9c9f55d8b
Robert Harris <therobertharris@gmail.com>
2013-02-23 22:29:20 -0500

    added clock_test code

---
+++ micro/testing/clock_test/src/asf/common/boards/user_board/init.c
@@ -0,0 +1,25 @@
+/**
+ * \file
+ *
+ * \brief User board initialization template
+ *
+ */
+
+#include <asf.h>
+#include <board.h>
+#include <conf_board.h>
+
+void board_init(void)
+{
+	ioport_init();
+	
+	ioport_enable_pin(LED);	
+	ioport_set_pin_dir(RGB_DATA, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(RGB_CLK, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(LED, IOPORT_DIR_OUTPUT);
+	
+	/* This function is meant to contain board-specific initialization code
+	 * for, e.g., the I/O pins. The initialization can rely on application-
+	 * specific board configuration, found in conf_board.h.
+	 */
+}
---
+++ micro/testing/clock_test/src/main.c
@@ -0,0 +1,53 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+#include <asf.h>
+#include <pll.h>
+#include <sysclk.h>
+#include <delay.h>
+void clkrgb(void);
+
+int main (void)
+{
+	sysclk_init();
+    board_init();
+	 struct pll_config pcfg;
+	 
+	osc_enable(OSC_ID_OSC0);
+	osc_wait_ready(OSC_ID_OSC0);
+	sysclk_set_source(SYSCLK_SRC_OSC0);
+	
+	 // pll_config_init(&pcfg, PLL_SRC_OSC0, 1,
+	 //  40000000 / BOARD_OSC0_HZ);
+	 //  pll_enable(&pcfg, 0);
+	 //  sysclk_set_prescalers(1,1,1);
+	 //  pll_wait_for_lock(0);
+	 // sysclk_set_source(SYSCLK_SRC_PLL0);
+	//sysclk_set_source(SYSCLK_SRC_OSC0);
+	
+	
+	while(1){
+		clkrgb();
+	}
+	
+	
+	// Insert application code here, after the board has been initialized.
+}
+
+void clkrgb(void)
+{
+	ioport_set_pin_level(RGB_CLK, IOPORT_PIN_LEVEL_HIGH);
+	delay_us(1);
+	//asm("nop");
+	ioport_set_pin_level(RGB_CLK, IOPORT_PIN_LEVEL_LOW);
+	delay_us(1);
+	//asm("nop");
+}
