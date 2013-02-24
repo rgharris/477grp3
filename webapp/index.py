a11eb04249cbe938cc1534151cb14748d88072d1
Spencer Julian <helloThere@spencerjulian.com>
2013-02-24 16:29:05 -0500

    Still trying to get the CSS right. I'll be making a lot of commits, as I'm working on a different machine than the pi.

--- webapp/index.py
+++ webapp/index.py
@@ -92,7 +92,7 @@ if True:
 			<link rel="stylesheet" href="styles/catronNormal.css" type="text/css" media="only screen and (min-device-width: 721px)/>-->
 			<style type="text/css">
 				@import url("styles/catronNormal.css");
-				@media screen and (max-device-width: 480px) and (orientation:portrait){
+				@media screen and (max-device-width: 360px) and (orientation:portrait){
 					@import url("styles/catronMobilePortrait.css");
 				}
 				@media screen and (max-device-width: 640px) and (orientation:landscape){
