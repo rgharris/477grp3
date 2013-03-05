fa19884e8840003a97ea15ed307ffa389cfb1820
Spencer Julian <helloThere@spencerjulian.com>
2013-03-05 15:28:47 -0500

    And now we begin the debugging show again.

--- webapp/dialogs/purchase.py
+++ webapp/dialogs/purchase.py
@@ -30,13 +30,13 @@ else:
 	elif pairs.has_key("confirm"):
 		output = "<form method=\"post\" action=\"index.py\">\n<h2>Confirm Purchase</h2>\n"
 		if pairs["confirm"][0] == "settle":
-			output.append("<p>Do you wish to purchase a settlement for 1 wood, 1 wheat, 1 sheep, and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"settle\" />")
+			output = output + "<p>Do you wish to purchase a settlement for 1 wood, 1 wheat, 1 sheep, and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"settle\" />"
 		elif pairs["confirm"][0] == "city":
-			output.append("<p>Do you wish to purchase a city for 2 wheat and 3 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"city\" />")
+			output = output + "<p>Do you wish to purchase a city for 2 wheat and 3 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"city\" />"
 		elif pairs["confirm"][0] == "road":
-			output.append("<p>Do you wish to purchase a road for 1 wood and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"road\" />")
+			output = output + "<p>Do you wish to purchase a road for 1 wood and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"road\" />"
 		elif pairs["confirm"][0] == "dev":
-			output.append("<p>Do you wish to purchase a development card for 1 wheat, 1 sheep, and 1 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"dev\" />")
+			output = output + "<p>Do you wish to purchase a development card for 1 wheat, 1 sheep, and 1 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"dev\" />"
 		output.append("<input type=\"submit\" value=\"Yes I do!\" class=\"bottom half left\" name=\"confirmPurchase\"/><input type=\"submit\" value=\"No I don't!\" class=\"bottom half right\" name=\"doNotPurchase\"/>")
 	elif pairs.has_key("place"):
 		output = "<h2>Place piece</h2>\n<p>Please place your piece now.</p><a href=\"index.py#x\" class=\"bottom\">Done.</a>"
