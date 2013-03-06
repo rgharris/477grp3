dc07445868108f95c86c3693063908fac566b114
Spencer Julian <helloThere@spencerjulian.com>
2013-03-05 22:19:36 -0500

    Found bug in status dialog, removed it.

--- webapp/dialogs/gameStatus.py
+++ webapp/dialogs/gameStatus.py
@@ -15,16 +15,17 @@ print
 print "<h2>Game Status</h2>\n"
 print "\t<ul class=\"gameStatus\">\n"
 for fn in os.listdir(PLAYER_FILE):
-	jsonInfo = open(PLAYER_FILE + fn)
-	playerInfo = json.load(jsonInfo)
-	print "\t\t<li><b>" + playerInfo["playerName"] + "</b> &nbsp; &nbsp; " + str(playerInfo["points"]) + " points"
-	if len(playerInfo["awards"]) > 0:
-		if len(playerInfo["awards"]) == 1 and playerInfo["awards"][0] == "road":
-			print "<br />\n\t\t<i>Has the longest road</i>"
-		elif len(playerInfo["awards"]) == 1 and playerInfo["awards"][0] == "army":
-			print "<br />\n\t\t<i>Has the largest army</i>"
-		elif len(playerInfo["awards"]) == 2:
-			print "<br />\n\t\t<i>Has the longest road and the largest army</i>"
-	print "</li>\n"
-	jsonInfo.close()
+	if fn != 'dev.json':
+		jsonInfo = open(PLAYER_FILE + fn)
+		playerInfo = json.load(jsonInfo)
+		print "\t\t<li><b>" + playerInfo["playerName"] + "</b> &nbsp; &nbsp; " + str(playerInfo["points"]) + " points"
+		if len(playerInfo["awards"]) > 0:
+			if len(playerInfo["awards"]) == 1 and playerInfo["awards"][0] == "road":
+				print "<br />\n\t\t<i>Has the longest road</i>"
+			elif len(playerInfo["awards"]) == 1 and playerInfo["awards"][0] == "army":
+				print "<br />\n\t\t<i>Has the largest army</i>"
+			elif len(playerInfo["awards"]) == 2:
+				print "<br />\n\t\t<i>Has the longest road and the largest army</i>"
+		print "</li>\n"
+		jsonInfo.close()
 print"</ul>\t<a href=\"#x\" class=\"bottom\">Got it!</a>"
