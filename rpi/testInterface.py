9f4b252b536f6ccc8800fff28f0832cf46276e76
Spencer Julian through RPI <smjulian@purdue.edu>
2013-03-11 20:55:52 +0000

    Completed test interface terminal. Needs to be tested, but I am currently unable. Haven't even tried to run it in python, yet.

--- rpi/testInterface.py
+++ rpi/testInterface.py
@@ -4,30 +4,76 @@
 #the microcontroller over i2c.
 #
 #Parameters: address of the i2c slave.
-import time, os, sys
-
-#Use Adafruit's rPI library for GPIO,
-#as it has better interrupt handling.
-import rPI.GPIO as gpio
-
-#Use quick2wire's I2C libraries,
-#as they seem easier to use and more
-#robust.
+import time, os, sys, select
 import quick2wire.i2c as i2c
+from quick2wire.gpio import Pin, In, Out, Both
+from quick2wire.selector import Selector
 
-bus = smbus.SMBus(1)
+#The pin used to throw an I2C interrupt
+GPIOPIN = 4
+#Register that tells us how many bytes to read
+#when we're reading from I2C. Should only be
+#one byte long.
+NBYTEREGISTER = 0xFE
+#The register and value we need to write
+#to in order to tell the micro that we've
+#successfully read the information.
+CLEARREGISTER = 0xFF
+CLEARVALUE = 0x00
+#This register is the starting register to read from
+STARTREGISTER = 0x00
 
-def i2cReceiveData():
-
-def runTerminal(address):
-	#Setup GPIO
-	gpio.setmode(gpio.BOARD)
-	gpio.setup(4, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-
-	#Start loop as I2C Master. Loops infinitely.
+def i2cReadData(address):
+	print "Receiving data..."
 	with i2c.I2CMaster() as bus:
-		gpio.add_event_detect(4,gpio.RISING, i2cReceiveData)
+		#First, read to see how many bytes to read. Yeah, I know...
+		read_num = bus.transaction(i2c.writing_bytes(address, NBYTEREGISTER), i2c.reading(address, 1))
+		#Now we read that many bytes, starting at the first register
+		read_result = bus.transaction(i2c.writing_bytes(address, STARTREGISTER), i2c.reading(address, read_num))
+	print read_result
+
+def i2cWriteData(address):
+	print "Type q to quit writing."
+	tosend = raw_input('To Microcontroller (format is register:bytes):')
+	while (tosend != "q"):
+		register,bytes = tosend.split(':',1)
+		#Setup I2C Master object to set RPI as master
+		with i2c.I2CMaster() as bus:
+			bus.transaction(i2c.writing(address,register + bytes))
+		print "Complete."
+		print
+		print "Press any key to send command, CTRL+C to exit."
 	
+def runTerminal(address):
+	#Setup terminal
+	print "Press any key to send command, CTRL+C to exit."
+	dataFlag = HeaderPin(GPIOPIN, Pin.In, Pin.Rising)
+	#Setup epoll, sort of like C's select statement
+	epoll = select.epoll()
+	#Register the pin, wait for level changes
+	epoll.register(dataFlag, select.EPOLLIN | select.EPOLLET)
+	#Register standard input (the keyboard), wait for input
+	#Since we're going to do something on keypress, we won't be able
+	#to grab events from the micro while typing. This is fine, because
+	#it won't be an issue in the interface - reading and writing will be
+	#independant.
+	#
+	#Should we just have a function in the "ttl" file (which doesn't exist yet)
+	#that's called on the web interface that checks the GPIO level? If so,
+	#that would make things easier (no need to write a daemon for this), but
+	#it would mean there's up to 5 seconds (or whatever the ttl refresh value is)
+	#where the pi could not react to the micro.
+	#Something to think about.
+	epoll.register(sys.stdin, select.EPOLLIN)
+	#Endless loop waiting for something.
+	while True:
+		events = epoll.poll()
+		for fileno, event in events:
+			if fileno == sys.stdin.fileno():
+				i2cWriteData(address)
+			if fileno == pin.fileno():
+				i2cReadData(address)
+
 if __name__ == "__main__":
 	if (len(sys.argv) != 2):
 		print "Usage: " + sys.argv[0] + " address"
