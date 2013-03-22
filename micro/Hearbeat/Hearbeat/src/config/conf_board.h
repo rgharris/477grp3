8c43d577cb183c88f9b7d5d93c8e97a7e9f13782
Joshua A Hunsberger <joshua.a.hunsberger@gmail.com>
2013-03-22 01:59:11 -0400

    Changed the clocks to run on oscillator, not pll

--- micro/Hearbeat/Hearbeat/src/config/conf_board.h
+++ micro/Hearbeat/Hearbeat/src/config/conf_board.h
@@ -10,11 +10,11 @@
 
 #define BOARD_OSC0_HZ			BOARD_XOSC_HZ
 #define BOARD_OSC0_IS_XTAL		false
-#define BOARD_OSC0_STARTUP_US	1100
+#define BOARD_OSC0_STARTUP_US	17000
 
 // This the HEARTBEAT.  Thats why this is named
 // HEARTBEAT project. HEAR DA HEARTBEATBEAT
-#define	CLKOUT					AVR32_PIN_PA11
+#define	CLKOUT					AVR32_PIN_PB02
 
 
 #endif // CONF_BOARD_H
