eedbac708e746ce6a6614c7cf0742a8357b63b62
Spencer Julian <helloThere@spencerjulian.com>
2013-04-02 22:09:43 -0400

    Still working on trading.

--- webapp/dialogs/trade.py
+++ webapp/dialogs/trade.py
@@ -37,49 +37,41 @@ if len(pairs) == 0:
 					<option value="wheat">Wheat</option>
 					<option value="sheep">Sheep</option>
 					<option value="wood">Wood</option></select>
-				From:
-				<select class="modalSelect"> {0}
-				</select>
 				<input type="submit" value="No Deal!" class="bottom half left" name="noDeal" />
 				<input type="submit" value="Deal!" class="bottom half right" name="deal" />
         	   </form>
 			"""
-	for fn in os.listdir(PLAYER_FILE):
-		if fn != 'dev.json':
-			jsonInfo = open(PLAYER_FILE + fn)
-			playerInfo = json.load(jsonInfo)
-			playerList.append("<option value=\"" + playerInfo["playerName"] + "\">" + playerInfo["playerName"] + "</option>")
-			jsonInfo.close()
-	output = output.format('\n'.join(playerList))
 else:
 	if pairs.has_key("invalid"):
 		#Shows up on current /or/ remote player's screen to identify invalid.
-		humanMap = {"settle":"settlement", "city":"city", "road":"road", "dev":"development card"}
-		output = "<h2>Purchase Error</h2>\n<p>You don't have enough resources to purchase a " + humanMap[pairs["invalid"][0]] + "!</p><a href=\"index.py#x\" class=\"bottom\">Got it!</a>"
+		output = "<h2>Trade Error</h2>\n<p>You don't have enough resources to trade with " + pairs["invalid"][0] + "</p><a href=\"index.py#x\" class=\"bottom\">Got it!</a>"
+	elif pairs.has_key("valid"):
+		#Shows up on current player's screen to ask for player to trade with.
+		output = """<form method="post" action="index.py">
+				<h2>Trade</h2>
+				From:
+				<select class="modalSelect"> {0}
+				</select>
+				<input type="submit" value="Nevermind." class="bottom half left" name="noDeal" />
+				<input type="submit" value="Let's do it!" class="bottom half right" name="trade" />
+				</form>
+			"""
+		for fn in os.listdir(PLAYER_FILE):
+			if fn != 'dev.json':
+				jsonInfo = open(PLAYER_FILE + fn)
+				playerInfo = json.load(jsonInfo)
+				playerList.append("<option value=\"" + playerInfo["playerName"] + "\">" + playerInfo["playerName"] + "</option>")
+				jsonInfo.close()
+		output = output.format('\n'.join(playerList))
 	elif pairs.has_key("confirm"):
 		#Shows up on remote player's screen to confirm trade.
-		output = "<form method=\"post\" action=\"index.py\">\n<h2>Confirm Purchase</h2>\n"
-		if pairs["confirm"][0] == "settle":
-			output = output + "<p>Do you wish to purchase a settlement for 1 wood, 1 wheat, 1 sheep, and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"settle\" />"
-		elif pairs["confirm"][0] == "city":
-			output = output + "<p>Do you wish to purchase a city for 2 wheat and 3 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"city\" />"
-		elif pairs["confirm"][0] == "road":
-			output = output + "<p>Do you wish to purchase a road for 1 wood and 1 clay?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"road\" />"
-		elif pairs["confirm"][0] == "dev":
-			output = output + "<p>Do you wish to purchase a development card for 1 wheat, 1 sheep, and 1 ore?</p>\n<input type=\"hidden\" name=\"purchase\" value=\"dev\" />"
+		output = "<form method=\"post\" action=\"index.py\">\n<h2>Confirm Trade</h2>\n"
+		#Remember, all variables are from the perspective of the current player's turn, so this looks backwards but it is not.
+		output = output + "<p>Do you wish to trade " + pairs["forNum"] + " " + pairs["for"] + " for " + pairs["getNum"] + " " + pairs["get"] + "?</p>"
 		output = output + "<input type=\"submit\" value=\"Yes I do!\" class=\"bottom half left\" name=\"confirmPurchase\"/><input type=\"submit\" value=\"No I don't!\" class=\"bottom half right\" name=\"doNotPurchase\"/>"
 	elif pairs.has_key("deny"):
 		#Shows up on current player's screen to acknowledge denial of trade.
-		output = "<h2>Place piece</h2>\n<p>Please place your piece now.</p><!--TODO:This needs removed when we're communicating with the board.--><a href=\"index.py#x\" class=\"bottom\">Done.</a>"
-	elif pairs.has_key("obtained"):
-		humanMap = {"knights":"Knight", "monopoly":"Monopoly", "road":"Road Building", "plenty":"Year of Plenty", "victory":"Victory Point"}
-		if pairs["obtained"][0] != "victory":
-			output = "<h2>Development Card!</h2>\n<p>You have received a " + humanMap[pairs["obtained"][0]] + " card! You can play it any time, starting next turn, by selecting the \"Dev Cards\" item on the resource screen.</p><a href=\"index.py#x\" class=\"bottom\">Got it!</a>"
-		else:
-			output = "<h2>Victory Card!</h2>\n<p>Congratulations! You are one step closer to victory! You have obtained a victory point card. No one will see this victory point until the end of the game, and it will not show up in the status screen. It will only show up on your main point count next to your name.</p><a href=\"index.py#x\" class=\"bottom\">Awesome!</a>"
-	elif pairs.has_key("development"):
-		if pairs["development"][0] == "none":
-			output = "<h2>No development cards!</h2>\n<p>There are no development cards to draw! Pick another thing to purchase, if you are able.</p><a href=\"index.py#x\" class=\"bottom\">Got it.</a>"
+		output = "<h2>Trade denied</h2>\n<p>Your trade has been denied from the player you were trading with.</p><a href=\"index.py#x\" class=\"bottom\">Got it!</a>"
 
 
 print output
