6b221f0583edaad8354c87fd213f823f1cd5353f
Spencer Julian <helloThere@spencerjulian.com>
2013-04-04 19:34:56 -0400

    Debugging trading now.

--- webapp/index.py
+++ webapp/index.py
@@ -316,7 +316,8 @@ elif "trade" in form:
 	tradePlayer = form.getvalue('playerid')
 	#Check if remote player can trade. If so, submit proper request, if not, submit cannot trade.
 	tradeInfo = readJson(TRADE_FILE)
-	if (chkResources(tradePlayer, {tradeInfo['get']['resource']:int(tradeInfo['get']['amount'])}) == False):
+	tradingPlayerInfo = readJson("players/" + tradePlayer + ".json")
+	if (chkResources(tradingPlayerInfo, {str(tradeInfo['get']['resource']):int(tradeInfo['get']['amount'])}) == False):
 		setRefresh(int(tradePlayer), REFRESH_VALUE['cannotTrade'])
 	else:
 		setRefresh(int(tradePlayer),REFRESH_VALUE['tradeRequest'])
