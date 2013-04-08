639c343f7f6386db4ba8a12f83f4e6e017ce89f6
Spencer Julian <helloThere@spencerjulian.com>
2013-04-08 19:43:17 -0400

    Debugging i2c

--- rpi/testInterface.py
+++ rpi/testInterface.py
@@ -27,33 +27,34 @@ def i2cReadData(address):
 	print("Receiving data...")
 	with i2c.I2CMaster() as bus:
 		#First, read to see how many bytes to read. Yeah, I know...
+		print i2c.reading(address, 1)
 		read_num = bus.transaction(i2c.writing_bytes(address, NBYTEREGISTER), i2c.reading(address, 1))
 		#Now we read that many bytes, starting at the first register
 		read_result = bus.transaction(i2c.writing_bytes(address, STARTREGISTER), i2c.reading(address, read_num))
 	print(read_result)
 
-def i2cWriteData(address):
-	print("Type q to quit writing.")
-	tosend = input('To Microcontroller (format is register:bytes):')
-	while (tosend != "q"):
-		register,bytes = tosend.split(':',1)
-		#Setup I2C Master object to set RPI as master
-		with i2c.I2CMaster() as bus:
-			bus.transaction(i2c.writing(address,register + bytes))
-		print("Complete.")
-		print()
-		print("Press any key to send command, CTRL+C to exit.")
+#def i2cWriteData(address):
+#	print("Type q to quit writing.")
+#	tosend = input('To Microcontroller (format is register:bytes):')
+#	while (tosend != "q"):
+#		register,bytes = tosend.split(':',1)
+#		#Setup I2C Master object to set RPI as master
+#		with i2c.I2CMaster() as bus:
+#			bus.transaction(i2c.writing(address,register + bytes))
+#		print("Complete.")
+#		print()
+#		print("Press any key to send command, CTRL+C to exit.")
 	
-def runTerminal(address):
+#def runTerminal(address):
 	#Setup terminal
-	print("Press any key to send command, CTRL+C to exit.")
+#	print("Press any key to send command, CTRL+C to exit.")
 	#dataFlag = Pin(GPIOPIN, direction=In, interrupt="rising")
-	dataFlag = pins.pin(GPIOPIN, In, Rising)
+#	dataFlag = pins.pin(GPIOPIN, In, Rising)
 	#Setup epoll, sort of like C's select statement
-	with dataFlag:
-		epoll = select.epoll()
+#	with dataFlag:
+#		epoll = select.epoll()
 		#Register the pin, wait for level changes
-		epoll.register(dataFlag, select.EPOLLIN | select.EPOLLET)
+	#	epoll.register(dataFlag, select.EPOLLIN | select.EPOLLET)
 		#Register standard input (the keyboard), wait for input
 		#Since we're going to do something on keypress, we won't be able
 		#to grab events from the micro while typing. This is fine, because
@@ -66,19 +67,19 @@ def runTerminal(address):
 		#it would mean there's up to 5 seconds (or whatever the ttl refresh value is)
 		#where the pi could not react to the micro.
 		#Something to think about.
-		epoll.register(sys.stdin, select.EPOLLIN)
+#		epoll.register(sys.stdin, select.EPOLLIN)
 		#Endless loop waiting for something.
-		while True:
-			events = epoll.poll()
-			for fileno, event in events:
-				if fileno == sys.stdin.fileno():
-					i2cWriteData(address)
-				if fileno == dataFlag.fileno():
-					i2cReadData(address)
+#		while True:
+#			events = epoll.poll()
+#			for fileno, event in events:
+#				if fileno == sys.stdin.fileno():
+#					i2cWriteData(address)
+#				if fileno == dataFlag.fileno():
+#					i2cReadData(address)
 
 if __name__ == "__main__":
 	if (len(sys.argv) != 2):
 		print("Usage: " + sys.argv[0] + " address")
 		print("Address should be in 0xYY format, with YY being the hex address.")
 	else:
-		runTerminal(sys.argv[1])
+		i2cReadData(sys.argv[1])
