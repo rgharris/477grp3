fa00f5e4a18e29a187a494b2f6e8e44ec281ba15
Spencer Julian <helloThere@spencerjulian.com>
2013-04-13 20:12:14 -0400

    Working on sending random values to the pi

--- rpi/catronBootup.py
+++ rpi/catronBootup.py
@@ -4,6 +4,7 @@
 
 import quick2wire.i2c as i2c
 import sys,os
+import time #debugging...
 import random as r
 
 MICROADDR = 0x50
@@ -17,6 +18,7 @@ if __name__ == "__main__":
 	random.seed() #Uses system time by default
 	with i2c.I2CMaster() as bus:
 		bus.transaction(i2c.writing_bytes(MICROADDR, STARTRAND, r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250))) #Too lazy to figure out the 'pythonic' way to do this. This should work. Repeated 19 times.
-		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, BOOTUP)
-		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, STARTGAME)
+		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, BOOTUP))
+		time.sleep(1)
+		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, STARTGAME))
 	print("Write attmpted. Completion not ensured.")
