96f0876ac910c0a5ccd2531ffe028ac06a61b552
Spencer Julian <helloThere@spencerjulian.com>
2013-04-13 20:11:07 -0400

    Working on sending random values to micro

--- rpi/catronBootup.py
+++ rpi/catronBootup.py
@@ -4,7 +4,6 @@
 
 import quick2wire.i2c as i2c
 import sys,os
-import time #debugging...
 import random as r
 
 MICROADDR = 0x50
@@ -19,6 +18,5 @@ if __name__ == "__main__":
 	with i2c.I2CMaster() as bus:
 		bus.transaction(i2c.writing_bytes(MICROADDR, STARTRAND, r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250), r.randint(0,250))) #Too lazy to figure out the 'pythonic' way to do this. This should work. Repeated 19 times.
 		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, BOOTUP)
-		time.sleep(1)
 		bus.transaction(i2c.writing_bytes(MICROADDR, PIREG, STARTGAME)
 	print("Write attmpted. Completion not ensured.")
