35c9e621305de968b04482b4fe2e49b38d9656cf
Spencer Julian <helloThere@spencerjulian.com>
2013-04-14 19:15:50 -0400

    Working on initial settlement placements

---
+++ webapp/dialogs/initSetup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# This is the dialog that deals with trading.
+# It should deal with both traders.
+#import debugging
+import cgitb
+#Everything else.
+import os, sys, cgi, json
+
+PLAYER_FILE = "../players/"
+GAME_STATE_FILE = "../chkRefresh/gamestate.json"
+
+query = os.environ["QUERY_STRING"]
+
+pairs = cgi.parse_qs(query)
+
+print("Content-type: text/html;charset=utf-8\n\n")
+
+if len(pairs) == 0:
+	output = """<h2>Error!</h2>\n<p>This should never happen.</p>"""
+
+else:
+	playerID = pairs['player'][0]
+	playerFile = open(PLAYER_FILE + playerID + ".json")
+	playerInfo = json.load(playerFile)
+	playerFile.close()
+	stateFile = open(GAME_STATE_FILE)
+	stateInfo = json.load(stateFile)
+	stateFile.close()
+	if playerInfo['currentTurn'] == 1:
+		if playerInfo['initialPlacements']['settlements'] == 0:
+			output = """<h2>Place Settlement.</h2>
+							<p>Place your settlement now.</p>
+						"""
+		if playerInfo['initialPlacements']['settlements'] == 1 and playerInfo['initialPlacements']['roads'] == 0:
+			output = """<h2>Place Road.</h2>
+							<p>Place your road now.</p>
+						"""
+		if playerInfo['initialPlacements']['settlements'] == 1 and playerInfo['initialPlacements']['roads'] == 1:
+			output = """<h2>Place Settlement.</h2>
+							<p>Place your second settlement now.</p>
+						"""
+		if playerInfo['initialPlacements']['settlements'] == 2 and playerInfo['initialPlacements']['roads'] == 1:
+			output = """<h2>Place Road.</h2>
+							<p>Place your second road now.</p>
+						"""
+	if playerInfo['currentTurn'] == 0:
+		output = """<h2>Waiting for turn</h2>
+						<p>Waiting for your turn to place a settlement.</p>"""
+print(output)
