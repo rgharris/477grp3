f443bac573297b6c058e4012400c3ec5dc136218
Spencer Julian <helloThere@spencerjulian.com>
2013-04-15 13:24:07 -0400

    debugging i2c in web interface

--- webapp/dialogs/i2c.py
+++ webapp/dialogs/i2c.py
@@ -21,9 +21,8 @@ if len(pairs) == 0:
 	output = """<h2>Error!</h2>\n<p>This should never happen.</p>"""
 
 else:
-	read = pairs['read'][0]
-	output = "<h2>Testing</h2>\n<p>confirm: " + str(pairs['confirm'][0]) + " and read: " + str(pairs['read'][0]) + "."
-	if pairs['confirm'][0] == 1:
+	read = int(pairs['read'][0])
+	if pairs['confirm'][0] == "1":
 		if read % 10 == 0:
 			piece = "thief"
 		elif read % 10 == 1:
@@ -33,7 +32,7 @@ else:
 		elif read % 10 == 3:
 			piece = "city"
 		output = "<form action=\"index.py\" method=\"post\"><h2>Confirm piece</h2>\n<p>Would you like to place a " + piece + " in the indicated location on the board?<input type=\"hidden\" value=\"" + piece + "\" name=\"pieceType\" /><input type=\"submit\" value=\"No I don't!\" class=\"bottom half left\" name=\"denyPiecePlacement\"/><input type=\"submit\" value=\"Yes I do!\" class=\"bottom half right\" name=\"confirmPiecePlacement\"/></form>"
-	elif pairs['confirm'][0] == 2:
+	elif pairs['confirm'][0] == "2":
 		if read < 30:
 			action = "remove"
 			if read % 20 == 0:
