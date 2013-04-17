f9904040487b921487f9bea8999282090e151e8e
Spencer Julian through RPI <smjulian@purdue.edu>
2013-04-17 02:43:35 +0000

    got template working with bottle

---
+++ webapp/layouts/layout.tpl
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  yayaya 
+</body>
+</html>
--- webapp/test.py
+++ webapp/test.py
@@ -1,10 +1,25 @@
 import bottle
-from bottle import get, post, request
+from bottle import get, post, request, static_file, template, TEMPLATE_PATH
+
+if '/home/pi/477grp3/webapp/layouts/' not in TEMPLATE_PATH:
+	TEMPLATE_PATH.insert(0,'/home/pi/477grp3/webapp/layouts/')
+
+@get('/images/<filename>')
+def serve_image(filename):
+	return static_file(filename, root='/home/pi/477grp3/webapp/images/')
+
+@get('/styles/style.css')
+def serve_stylesheet():
+	return static_file("style.css", root='/home/pi/477grp3/webapp/styles/')
 
 @get('/')
+def show_webapp():
+	return template('layout')
+
+@get('/blah')
 def show_form():
-    return '''\
-<img src="images/wood.png" />
+	return '''\
+<img src="/images/wood.png" />
 <form action="" method="POST">
     <label for="name">What is your name?</label>
     <input type="text" name="name"/>
@@ -13,7 +28,7 @@ def show_form():
 
 @post('/')
 def show_name():
-    return "Hello, {}!".format(request.POST.name)
+	return "Hello, {}!".format(request.POST.name)
 
 application=bottle.default_app()       # run in a WSGI server
 #bottle.run(host='localhost', port=8080) # run in a local test server
