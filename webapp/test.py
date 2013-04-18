c65b50f322912b08cb55f7b11b0b9d38ee0b7459
Spencer Julian <helloThere@spencerjulian.com>
2013-04-18 18:26:02 -0400

    Working on trading now

--- webapp/test.py
+++ webapp/test.py
@@ -231,7 +231,7 @@ def handle_ajax():
 		elif mid == "endTurn":
 			return template('endTurn')
 		elif mid == "trade":
-			return template('trade')
+			return template('trade', players=getGameInfo()['playerInfo'], newTrade=True)
 		elif mid == "purchase":
 			return "purchase"
 		elif mid == "devCards":
